c0dfc85edfd379699c0052d0f46f77d232b6b560
ArcidesFerrao <cidesferrao@gmail.com>
2024-10-24 17:46:31 +0200

    project page from [id]

---
+++ src/app/(portfolio)/projects/[id]/page.tsx
@@ -0,0 +1,43 @@
+import db from "@/db/db";
+import { notFound } from "next/navigation";
+import React from "react";
+
+export default async function ProjectPage({
+  params: { id },
+}: {
+  params: { id: string };
+}) {
+  const project = await db.project.findUnique({ where: { id } });
+
+  if (project == null) return notFound();
+
+  return (
+    <div className="page-container adjustSize">
+      <div className="head-project">
+        <h2>{project?.projectName}</h2>
+        <div className="url-container">{/* <a href={}>Explore</a> */}</div>
+      </div>
+
+      <div className="image-project">
+        <img src={project?.imagePath} alt="" />
+      </div>
+
+      <div className="overview-project sec">
+        <div className="over-title">
+          <h3 className="pr-page-title">Overview</h3>
+        </div>
+
+        <div className="over-block">
+          <p>{project?.description}</p>
+        </div>
+      </div>
+
+      <div className="tec-project sec">
+        <div className="technology-title">
+          <h3 className="pr-page-title">Technologies</h3>
+        </div>
+        <div className="technology-list">{project?.tech}</div>
+      </div>
+    </div>
+  );
+}
--- src/app/(portfolio)/projects/page.tsx
+++ src/app/(portfolio)/projects/page.tsx
@@ -1,5 +1,53 @@
-import React from "react";
+import db from "@/db/db";
+// import Image from "next/image";
+import Link from "next/link";
 
-export default function ProjectsPage() {
-  return <section className="projects-page flexDisplay"></section>;
+export default async function ProjectsPage() {
+  const projectsData = getProjects();
+
+  return (
+    <section className="projects-page flexDisplay">
+      <div className="projects-head">
+        <h2>Projects</h2>
+        <h4>{`I've worked on tons of little projects over the years but these are the ones that I'm most proud of.`}</h4>
+      </div>
+
+      <div className="projects adjustPadd">
+        {projectsData &&
+          (await projectsData).map((project, index) => (
+            <div className="project-container" key={index}>
+              <div className="project-image">
+                {/* <Image src={project.imagePath} alt={project.projectName} /> */}
+                <img src={project.imagePath} alt={project.projectName} />
+              </div>
+
+              <div className="project-detail">
+                <Link href={`/projects/${project.id}`}>
+                  <div className="project-name">
+                    <h3>{project.projectName}</h3>
+                  </div>
+                </Link>
+
+                <div className="project-description">
+                  <p className="description-text">{project.description}</p>
+                </div>
+              </div>
+            </div>
+          ))}
+      </div>
+    </section>
+  );
+}
+
+async function getProjects() {
+  const projects = await db.project.findMany({
+    select: {
+      id: true,
+      projectName: true,
+      description: true,
+      imagePath: true,
+    },
+  });
+
+  return projects;
 }
