ce568ed5edce9c917513362f98ffe2977185b5eb
ArcidesFerrao <cidesferrao@gmail.com>
2024-10-24 20:15:41 +0200

    add blog posts + blog page

---
+++ src/app/(portfolio)/blog/[id]/page.tsx
@@ -0,0 +1,81 @@
+import db from "@/db/db";
+import db
+import { notFound } from "next/navigation";
+import React from "react";
+
+export default async function BlogPost({
+  params: { id },
+}: {
+  params: { id: string };
+}) {
+  const postData = await db.post.findUnique({ where: { id } });
+
+  if (postData == null) return notFound();
+
+  return (
+    <section className="blog-post-container">
+      <div className="blog-block">
+        <div className="posted-date">
+          <span className="lets-icons--date-today-duotone"></span>
+          {postData.createdAt.toDateString()}
+        </div>
+
+        <div className="title-post shorten">
+          <h1 className="blog-title">{postData.title}</h1>
+        </div>
+
+        <div className="description-post shorten">
+          <p>{postData.description}</p>
+        </div>
+        <img className="blog-image" src={postData.imagePath} alt="" />
+
+        {/* <BlockContent 
+                className='post-block'
+                blocks={postData.body}
+                projectId={createClient.config.projectId}
+                dataset={createClient.config.dataset}
+            /> */}
+      </div>
+
+      <div className="author">
+        <div className="writtenBy down-border">
+          <h4>Written by: Arcides Ferrao</h4>
+        </div>
+
+        <div className="tags down-border">
+          <h3 className="auth-title">Tags</h3>
+          <div className="tag-container contain">
+            <div className="tags-post cont">
+              <p>Sanity</p>
+            </div>
+          </div>
+        </div>
+
+        <div className="share-post down-border">
+          <h3 className="auth-title">Share Post</h3>
+
+          <div className="icon-share contain">
+            <div className="icon-container cont">
+              <span className="bi--github icon-size"></span>
+            </div>
+            <div className="icon-container cont">
+              <span className="pajamas--twitter icon-size"></span>
+            </div>
+            <div className="icon-container cont">
+              <span className="bi--linkedin icon-size"></span>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="post-reference">
+        <h2>References</h2>
+        <div className="ref-link">
+          <ul>
+            <li></li>
+          </ul>
+        </div>
+      </div>
+    </section>
+  );
+}
--- src/app/(portfolio)/blog/page.tsx
+++ src/app/(portfolio)/blog/page.tsx
@@ -1,6 +1,11 @@
+// import Link from "next/link";
+import db from "@/db/db";
+import Link from "next/link";
 import React from "react";
 
-export default function BlogPage() {
+export default async function BlogPage() {
+  const posts = getPosts();
+
   return (
     <div className="blog-page adjustPadding">
       {/* <PostCategories /> */}
@@ -13,7 +18,62 @@ export default function BlogPage() {
             feel free to leave your thought in the comments.
           </h4>
         </div>
+
+        <div className="posts">
+          {posts &&
+            (await posts).map((post, index) => (
+              <div className="post cont" key={index}>
+                <div className="post-img">
+                  <img src={post.imagePath} alt="" className="post-image" />
+                </div>
+
+                <div className="post-detail">
+                  <div className="post-title">
+                    <Link href={`/post/${post.id}`}>
+                      <h3>{post.title}</h3>
+                    </Link>
+                  </div>
+
+                  <div className="post-description">
+                    <p className="post-preview-text">{post.description}</p>
+                  </div>
+
+                  <div className="post-date">
+                    <div className="date-posted">
+                      <h4>
+                        {post.createdAt.toDateString()}
+                        {/* {format(post.publishAt, "PPP")} */}
+                        {/* { datePost => {setDatePost(format(post.publishAt, "PPP"))} } */}
+                      </h4>
+                    </div>
+                    <div className="post-share">
+                      <a href="#">
+                        <span className="quill--share"></span>
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            ))}
+        </div>
       </section>
     </div>
   );
+}
+
+async function getPosts() {
+  const posts = await db.post.findMany({
+    select: {
+      id: true,
+      title: true,
+      createdAt: true,
+      description: true,
+      intro: true,
+      outro: true,
+      imagePath: true,
+      references: true,
+    },
+  });
+
+  return posts;
 }
