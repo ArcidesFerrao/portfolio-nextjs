afa95a7cb2f41a090defd7ee7cd856517aec69dd
ArcidesFerrao <cidesferrao@gmail.com>
2024-11-06 17:28:14 +0200

    add uploadthing

--- src/app/admin/_actions/projects.ts
+++ src/app/admin/_actions/projects.ts
@@ -2,45 +2,74 @@
 
 import db from "@/db/db";
 import { z } from "zod";
-import fs from "fs/promises"
+// import fs from "fs/promises"
 import { redirect } from "next/navigation";
+import { parseWithZod } from "@conform-to/zod";
 
-const fileSchema = z.instanceof(File, { message: "Required"})
+// const fileSchema = z.instanceof(File, { message: "Required"})
 
-const imageSchema = fileSchema.refine(file => file.size === 0 || file.type.startsWith("image/"))
+// const imageSchema = fileSchema.refine(file => file.size === 0 || file.type.startsWith("image/"))
 
 const addSchema = z.object({
     projectName: z.string().min(1),
     description: z.string().min(1),
     projectLink: z.string().min(1),
     tech: z.string().min(1),
-    image: imageSchema.refine(file => file.size> 0, "Required"),
+    image: z.string().min(1, "At least one image is required"),
+    // image: imageSchema.refine(file => file.size> 0, "Required"),
 })
 
-export async function addProject(prevState: unknown ,formData: FormData) {
-    const result = addSchema.safeParse(Object.fromEntries(formData.entries()))
+export async function addProject(prevState: unknown, formData: FormData) {
+    const submission = parseWithZod(formData, { schema: addSchema,});
 
-    if(result.success === false) {
-        return result.error.formErrors.fieldErrors;
+    if(submission.status !== "success") {
+        return submission.reply();
     }
-    // console.log(formData);
-    const data = result.data;
-    // console.log(result);
 
-    await fs.mkdir("public/projects", {recursive: true})
-    const imagePath = `/projects/${crypto.randomUUID()}-${data.image.name}`
-    await fs.writeFile(`public${imagePath}`, Buffer.from(await data.image.arrayBuffer()))
-    
+    // const flatten = submission.value.image.flatMap((urlString) => urlString.split(",").map((url) => url.trim()));
+
     await db.project.create({
-        data: {
-            authorId:"8a2dc2eb-fe1d-48fa-a1e9-f7dca70d2684",
-            projectName: data?.projectName,
-            description: data?.description,
-            tech: data?.tech,
-            projectLink: data.projectLink,
-            imagePath,
-        }
-    })
+                data: {
+                    authorId:"8a2dc2eb-fe1d-48fa-a1e9-f7dca70d2684",
+                    projectName: submission.value.projectName,
+                    description: submission.value.description,
+                    tech: submission.value.tech,
+                    projectLink: submission.value.projectLink,
+                    imagePath: submission.value.image,
+                }
+            })
 
     redirect("/admin/projects");
+
 }
+
+
+
+// export async function addProject(prevState: unknown ,formData: FormData) {
+//     const result = addSchema.safeParse(Object.fromEntries(formData.entries()))
+
+//     if(result.success === false) {
+//         return result.error.formErrors.fieldErrors;
+//     }
+//     // console.log(formData);
+//     const data = result.data;
+//     // console.log(result);
+
+//     await fs.mkdir("public", {recursive: true})
+//     await fs.mkdir("public/projects", {recursive: true})
+//     const imagePath = `/projects/${crypto.randomUUID()}-${data.image.name}`
+//     await fs.writeFile(`public${imagePath}`, Buffer.from(await data.image.arrayBuffer()))
+    
+//     await db.project.create({
+//         data: {
+//             authorId:"8a2dc2eb-fe1d-48fa-a1e9-f7dca70d2684",
+//             projectName: data?.projectName,
+//             description: data?.description,
+//             tech: data?.tech,
+//             projectLink: data.projectLink,
+//             imagePath,
+//         }
+//     })
+
+//     redirect("/admin/projects");
+// }
--- src/app/admin/_components/ProjectForm.tsx
+++ src/app/admin/_components/ProjectForm.tsx
@@ -3,35 +3,63 @@
 import { useFormStatus } from "react-dom";
 import { addProject } from "../_actions/projects";
 import { useActionState } from "react";
+import { UploadDropzone } from "@/app/lib/uploadthing";
+import { useForm } from "@conform-to/react";
+import { parseWithZod } from "@conform-to/zod";
+import { z } from "zod";
+
+const addSchema = z.object({
+  projectName: z.string().min(1),
+  description: z.string().min(1),
+  projectLink: z.string().min(1),
+  tech: z.string().min(1),
+  image: z.string().min(1, "At least one image is required"),
+  // image: imageSchema.refine(file => file.size> 0, "Required"),
+});
 
 export default function ProjectForm() {
-  const [error, action] = useActionState(addProject, {});
+  // const [error, action] = useActionState(addProject, {});
+  const [lastResult, action] = useActionState(addProject, undefined);
+  const [form, fields] = useForm({
+    lastResult,
+    onValidate({ formData }) {
+      return parseWithZod(formData, { schema: addSchema });
+    },
+    shouldValidate: "onBlur",
+    shouldRevalidate: "onInput",
+  });
   return (
-    <form action={action} className="project-form">
+    <form
+      id={form.id}
+      onSubmit={form.onSubmit}
+      action={action}
+      className="project-form"
+    >
       <div>
         <label htmlFor="projectName">Name</label>
         <input type="text" name="projectName" id="name" required />
-        {error.projectName && <div>{error.projectName}</div>}
+        {fields.projectName.errors && <div>{fields.projectName.errors}</div>}
       </div>
       <div>
         <label htmlFor="description">Description</label>
         <input type="text" name="description" id="description" required />
-        {error.description && <div>{error.description}</div>}
+        {/* {error.description && <div>{error.description}</div>} */}
       </div>
       <div>
         <label htmlFor="projectLink">Project Link</label>
         <input type="text" name="projectLink" id="projectLink" required />
-        {error.projectLink && <div>{error.projectLink}</div>}
+        {/* {error.projectLink && <div>{error.projectLink}</div>} */}
       </div>
       <div>
         <label htmlFor="tech">Technologies</label>
         <input type="text" name="tech" id="tech" required />
-        {error.tech && <div>{error.tech}</div>}
+        {/* {error.tech && <div>{error.tech}</div>} */}
       </div>
       <div>
         <label htmlFor="image">Image path</label>
-        <input type="file" name="image" id="image" required />
-        {error.image && <div>{error.image}</div>}
+        {/* <input type="file" name="image" id="image" required /> */}
+        <UploadDropzone endpoint="imageUploader" />
+        {/* {error.image && <div>{error.image}</div>} */}
       </div>
 
       <SubmitButton />
